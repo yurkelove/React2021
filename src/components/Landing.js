680a0c903b9de2e112cd8603f9fc93a19feb5c9a
Yura Kuzkin <yurkelove28@gmail.com>
2021-01-31 21:44:08 +0200

    Add component Landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,30 +3,57 @@ import React from 'react';
 import restaraunts from '../sample-restaurants';
 
 class Landing extends React.Component {
-    render() {
+    state = {
+        display: false,
+        title: "",
+        url: ""
+    };
 
+    displayList = () => {
+     const { display } = this.state;
+
+     this.setState({display: !display});
+    }
+
+    // componentDidMount() {
+
+    // }
+
+    getTitle = restaraunt => {
+        const {title, url } = restaraunt;
+
+        this.setState({title,url,display:false})
+    }
+
+    goToRestaurant = () => {
+        console.log('go to restaurant');
+    }
+
+    render() {
         return (
             <React.Fragment>
                 <div className="restaurant-select">
                     <div className="restaraunt-select__top">
-                        <div className="restraunt-select__top-header">
-                            Выбери ресторан
+                        <div onClick={this.displayList} className="restraunt-select__top-header font-effect-outline">
+                           { this.state.title ? this.state.title : 'Выбери ресторан' }
                         </div>
                         <div className="arrow-picker">
                             <div className="arrow-picker__up"></div>
                             <div className="arrow-picker__down"></div>
                         </div>
                     </div>
-                    <div className="restaurant-select__bottom">
+                    {this.state.display ? ( <div className="restaurant-select__bottom">
                         <ul>
                             {
-                                 restaraunts.map(restaraunts => {
-                                    return <li key={restaraunts.id}>{restaraunts.title}</li>
+                                 restaraunts.map(restaraunt => {
+                                    return <li onClick={() => this.getTitle(restaraunt)} key={restaraunt.id}>{restaraunt.title}</li>
                                 })
                             }
                         </ul>
                     </div>
-                    <button>Перейти в ресторан</button>
+                    ) : null
+                    }
+                    {this.state.title && !this.state.display ? (<button onClick={this.goToRestaurant}>Перейти в ресторан</button>) : null}
                 </div>
             </React.Fragment>
         )
